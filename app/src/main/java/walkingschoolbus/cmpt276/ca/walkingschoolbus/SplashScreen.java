5ab3d9a2a6ea0d50c6776909b8e72585699b34f7
Kawai <ksim@sfu.ca>
2018-02-27 16:39:44 -0800

    Initial Login set up

---
+++ app/src/main/java/walkingschoolbus/cmpt276/ca/walkingschoolbus/SplashScreen.java
@@ -0,0 +1,31 @@
+package walkingschoolbus.cmpt276.ca.walkingschoolbus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME = 6000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+
+                    sleep(SPLASH_SCREEN_TIME);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
