ce1b99c42e3886be4d7c6cbb8522c3ea44fccdeb
Kawai <ksim@sfu.ca>
2018-04-07 17:42:00 -0700

    fixed bug regarding getting all achievements

--- app/src/main/java/walkingschoolbus/cmpt276/ca/appUI/RewardsActivity.java
+++ app/src/main/java/walkingschoolbus/cmpt276/ca/appUI/RewardsActivity.java
@@ -18,27 +18,21 @@ import walkingschoolbus.cmpt276.ca.walkingschoolbus.R;
  */
 public class RewardsActivity extends AppCompatActivity {
 
-    int distanceWalked = 0;
+    int distanceWalked;
     User userManager = User.getInstance();
 
     ListView rewardsListView;
-//    String[] milestoneCompleted = {"3", "6", "8", "12","16", "24","32", "52"};
-//    Integer[] achievedStickers = {R.drawable.week3,R.drawable.week6,R.drawable.week8,R.drawable.week12,R.drawable.week16,R.drawable.week24,R.drawable.week32,R.drawable.week52};
 
     String[] milestone = {"5","15","10","20","50","75","100","125","150","200","500"};
     Integer[] milestoneStickers = {R.drawable.walk5,R.drawable.walk15,R.drawable.walk10,R.drawable.walk20,
-            R.drawable.walk50,R.drawable.walk75,R.drawable.walk125,R.drawable.walk150,R.drawable.walk200,R.drawable.walk500};
+            R.drawable.walk50,R.drawable.walk75,R.drawable.walk100,R.drawable.walk125,R.drawable.walk150,R.drawable.walk200,R.drawable.walk500};
     Integer[] notAchievedSticker = {R.drawable.notwalk5km,R.drawable.notwalk10,R.drawable.notwalk15,R.drawable.notwalk20,
             R.drawable.notwalk50,R.drawable.notwalk75,R.drawable.notwalk100,R.drawable.notwalk125,R.drawable.notwalk150,R.drawable.notwalk200,R.drawable.notwalk500,};
-//    int[] rewardsMilestone = {5,10,20,50,75,100};
 
     Achievement[] achievements = new Achievement[milestone.length];
 
     String[] milestoneCompleted = new String[milestone.length];
     Integer[] achievedStickers = new Integer[milestone.length];
-
-//    List<Achievement> achievementList = new ArrayList<>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +58,14 @@ public class RewardsActivity extends AppCompatActivity {
             Achievement newAchievement;
             if(distanceWalked >= Integer.parseInt(milestone[i])) {
                 newAchievement = new Achievement(milestone[i], milestoneStickers[i],"UNLOCKED");
+
+                System.out.println(""+ milestone.length + " | " + milestoneStickers.length);
+                achievements[i] = newAchievement;
             }
             else {
                 newAchievement = new Achievement(milestone[i], notAchievedSticker[i], "LOCKED");
+                achievements[i] = newAchievement;
             }
-            achievements[i] = newAchievement;
         }
     }
 
